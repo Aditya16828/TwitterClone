7536ad220e8251349113dd9fc9d8415f11ff2032
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2023-02-04 20:36:12 +0530

    Added tweet services and implementing hashtagging facility

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,43 +1,49 @@
-const {TweetRepository, HashtagRepository} = require('../repository/index');
+const { TweetRepository, HashtagRepository } = require("../repository/index");
 
-class Tweet_Service {
-    constructor(){
+class TweetService {
+    constructor() {
         this.tweetrepo = new TweetRepository();
         this.hashtagrepo = new HashtagRepository();
     }
 
-    async create(data){
-        const content = data.content;
+    async createTweet(data) {
+        try {
+            const content = data.content;
 
-        // find for hashtags using regex
-        let tags = content.match(/#[a-zA-Z0-9_]+/g);
-        tags = tags.map((tag) => tag.substring(1));
+            // find for hashtags using regex
+            let tags = content.match(/#[a-zA-Z0-9_]+/g);
+            tags = tags.map((tag) => tag.substring(1));
 
-        // console.log(tags);
+            // console.log(tags);
 
-        let oldtags = await this.hashtagrepo.findByName(tags);
-        let oldtagstitle = [];
-        oldtagstitle = oldtags.map(tag => tag.title);
-        // console.log("Old Tags:", oldtags);
-        let newtags = tags.filter(tag => (!oldtagstitle.includes(tag)));
-        // console.log("new Tags:", newtags);
+            let oldtags = await this.hashtagrepo.findByName(tags);
+            let oldtagstitle = [];
+            oldtagstitle = oldtags.map((tag) => tag.title);
+            // console.log("Old Tags:", oldtags);
+            let newtags = tags.filter((tag) => !oldtagstitle.includes(tag));
+            // console.log("new Tags:", newtags);
 
-        const tweet = await this.tweetrepo.createTweet(data);
+            const tweet = await this.tweetrepo.create(data);
 
-        newtags = newtags.map(tag => {
-            return {title: tag, tweets: [tweet.id]};
-        });
+            newtags = newtags.map((tag) => {
+                return { title: tag, tweets: [tweet.id] };
+            });
 
-        oldtags.forEach(tag => {
-            tag.tweets.push(tweet.id);
-            tag.save();
-        })
+            oldtags.forEach((tag) => {
+                tag.tweets.push(tweet.id);
+                tag.save();
+            });
 
-        const response = await this.hashtagrepo.createHashtags(newtags);
-        
-        // console.log(response);
-        return tweet;
+            const response = await this.hashtagrepo.createHashtags(newtags);
+
+            // console.log(response);
+            return { response, tweet };
+        } catch (error) {
+            // error = { ...error, fromLayer2: "Service" };
+            console.log(error);
+            throw error;
+        }
     }
 }
 
-module.exports = Tweet_Service;
+module.exports = TweetService;
